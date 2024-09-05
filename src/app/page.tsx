9d47ca9b880836f6acf89dca4d6b4662da2f443f
fauji2317 <114175922+fauji2317@users.noreply.github.com>
2024-09-05 19:17:31 +0700

    basic next

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // Define your props here
+}
+
+const MyComponent: React.FC<Props> = (props) => {
+  return (
+    <div>
+      {/* Your component content goes here */}
+    </div>
+  );
+};
+
+export default MyComponent;
