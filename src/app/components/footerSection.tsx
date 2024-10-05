0b0cf6197b1a327a093a9c0031872833390b7f0c
fauji2317 <114175922+fauji2317@users.noreply.github.com>
2024-10-05 08:37:10 +0700

    perbaikan minor

--- src/app/components/footerSection.tsx
+++ src/app/components/footerSection.tsx
@@ -7,21 +7,22 @@ const FooterSection = () => {
     <footer className="bg-white rounded-lg shadow m-4 mt-40">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="#"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <Image
-              src="/logo.png"
-              className="h-8"
-              alt="Flowbite Logo"
-              width={50} // Sesuaikan ukuran gambar sesuai kebutuhan
-              height={32}
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              CV Cipta Prima Engineering
-            </span>
-          </a>
+        <a
+          href="#"
+          className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+        >
+          <Image
+            src="/logo.png"
+            className="h-8"
+            alt="Flowbite Logo"
+            width={50} // Sesuaikan ukuran gambar sesuai kebutuhan
+            height={32}
+          />
+          <span className="text-lg sm:text-base md:text-xl font-semibold whitespace-nowrap">
+            CV Cipta Prima Engineering
+          </span>
+        </a>
+
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
             <li>
               <a href="#about-us" className="hover:underline me-4 md:me-6">About</a>
